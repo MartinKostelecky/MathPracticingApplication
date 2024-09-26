068cb44b0bd8f710e90ed1afd1a989bdef2de8db
MartinKostelecky <kosteleckymartin@gmail.com>
2024-09-26 21:04:26 +0200

    Authentication using Spring security + login.html added

---
+++ src/main/java/cz/martinkostelecky/mathpracticing/controller/AuthController.java
@@ -0,0 +1,58 @@
+package cz.martinkostelecky.mathpracticing.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    @RequestMapping(value = "/login", method = GET)
+    public String loginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/authenticate", method = POST)
+    public String authenticate(@RequestParam("username") String username,
+                               @RequestParam("password") String password,
+                               HttpServletRequest httpServletRequest,
+                               HttpServletResponse httpServletResponse,
+                               RedirectAttributes redirectAttributes) {
+
+        if (!passwordEncoder.matches(password, userDetailsService.loadUserByUsername(username).getPassword())) {
+            redirectAttributes.addAttribute("error", "Chybné uživatelské jméno nebo heslo.");
+            return "redirect:/login";
+        }
+
+        var authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+        SecurityContext context = SecurityContextHolder.getContext();
+
+        context.setAuthentication(authentication);
+
+        new HttpSessionSecurityContextRepository().saveContext(context, httpServletRequest, httpServletResponse);
+
+        redirectAttributes.addAttribute("success");
+        return "redirect:/examples";
+    }
+
+}
---
+++ src/main/java/cz/martinkostelecky/mathpracticing/security/SecurityConfig.java
@@ -0,0 +1,73 @@
+package cz.martinkostelecky.mathpracticing.security;
+
+import cz.martinkostelecky.mathpracticing.exception.UnauthorizedHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UnauthorizedHandler unauthorizedHandler;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
+                        httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(unauthorizedHandler))
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/login", "/authenticate", "/practicing")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.invalidSessionUrl("/login?expired")
+                                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                                .maximumSessions(2)
+                                .maxSessionsPreventsLogin(true))
+                .formLogin(login -> login.loginPage("/login"))
+                .logout(logout -> logout.deleteCookies("JSESSIONID"));
+
+        return httpSecurity.build();
+    }
+
+    //Uncomment and choose password
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("admin")
+                //.password(passwordEncoder().encode(""))
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
+        return builder.build();
+    }
+
+}
